67429bc57635addfe8630347dbbe33137a06fc7c
ajamalirad <aplasticlanguage@gmail.com>
2021-04-09 18:42:56 -0400

    started Buttons component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Buttons from './Buttons';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>airhead</h1>
+        <p>A meditation app to help you meditate</p>
       </header>
+
+        <Buttons />
+{/* buttons */}
+{/* will be replaced by meditation animation and timer */}
+{/* button appears on top of animation, as animation fades in background - opacity */}
+
     </div>
   );
 }
---
+++ src/Buttons.js
@@ -0,0 +1,14 @@
+const Buttons = () => {
+    return(
+        <div className="buttons">
+            <h2>take a breath</h2>
+            <p>A quote will appear to inspire your meditation. There will be a visual point of focus.</p>
+            <p>Select your preferred time below:</p>
+            <button>3 mins</button>
+            <button>5 mins</button>
+            <button>10 mins</button>
+        </div>
+    )
+}
+
+export default Buttons;
